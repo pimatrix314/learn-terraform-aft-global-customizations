72ca46f49d96eb35f56dfc781274b07e4b50716e
vikas <dvikas793@gmail.com>
2024-04-20 13:29:10 +0530

    fix error

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -8,6 +8,6 @@ provider "aws" {
   alias  = "aft_management_account_admin"
   region = "ap-south-1"
   assume_role {
-     role_arn = "arn:aws:iam::$data.aws_caller_identity.aft_management_account.account_id}:role/AWSAFTExecution"
+     role_arn = "arn:aws:iam::${data.aws_caller_identity.aft_management_account.account_id}:role/AWSAFTExecution"
   }
 }
